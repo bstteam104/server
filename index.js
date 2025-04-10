7a67dc91196612d5482c5b20e541402ec36c3860
bstte <bstteam104@gmail.com>
2025-04-10 18:50:29 +0530

    cors options updated

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Security & other middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://mundi-business.netlify.app'], 
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
 app.use(helmet());
 app.use(cookieParser());
 
